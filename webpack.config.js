afd15cef7d0c08a12c75493c2a4c307477e110fe
vitalii_denysiuk <dendoc97@gmail.com>
2017-05-19 10:27:08 +0300

    add sidebar

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: './src/js/main',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/',
     filename: 'build.js',
     library: 'app'
   },
